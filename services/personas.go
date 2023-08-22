985312aab5b79fb0016ff0c8d26a8f92bce3058c
sInwinkelried <101613134+Inwinkelried@users.noreply.github.com>
2023-08-22 16:09:06 -0300

    Update personas.go

--- services/personas.go
+++ services/personas.go
@@ -4,20 +4,22 @@ import "github.com/maxilovera/go-crud-example/dto"
 
 var personas []*dto.Persona
 
+//GET
 func ObtenerPersonas() []*dto.Persona {
 	return personas
 }
 
+//GET
 func ObtenerPersonaPorId(id int) *dto.Persona {
 	for _, persona := range personas {
 		if persona.ID == id {
 			return persona
 		}
 	}
-
 	return nil
 }
 
+//POST
 func CrearPersona(nuevaPersona dto.Persona) *dto.Persona {
 
 	nuevaPersona.ID = len(personas) + 1
@@ -25,4 +27,29 @@ func CrearPersona(nuevaPersona dto.Persona) *dto.Persona {
 	personas = append(personas, &nuevaPersona)
 
 	return &nuevaPersona
+}
+
+//PUT
+func ActualizarPersona(personaActualizada dto.Persona) *dto.Persona {
+	for _, persona := range personas {
+		if persona.ID == personaActualizada.ID {
+			persona.Nombre = personaActualizada.Nombre
+			persona.Edad = personaActualizada.Edad
+
+			return persona
+		}
+	}
+
+	return nil
+}
+
+//DELETE
+func EliminarPersona(id int) bool {
+	for i, persona := range personas {
+		if persona.ID == id {
+			personas = append(personas[:i], personas[i+1:]...)
+			return true
+		}
+	}
+	return false
 }
